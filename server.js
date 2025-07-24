9f2b8f0289fe77b1adb1b0a621bf81eedb571da5
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-24 04:57:53 +0300

    use TMDB for fetching movies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 
 

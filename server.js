9c140c1644f89c9df2745cf4adff1f4f9d852332
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-24 05:36:57 +0300

    implement search feature

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth",protectRoute, authRoutes);
 app.use("/api/v1/movie",protectRoute, movieRoutes);
 app.use("/api/v1/tv",protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 
 

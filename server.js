54aa5caf8a281501f7a86c421ce1eb21c9a7e34f
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-24 05:27:54 +0300

    add middleware to protect routes

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/auth",protectRoute, authRoutes);
+app.use("/api/v1/movie",protectRoute, movieRoutes);
+app.use("/api/v1/tv",protectRoute, tvRoutes);
 
 
 

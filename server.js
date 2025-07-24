412440f08a2702149fa2c311d718220077cc7f27
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-24 05:25:34 +0300

    add tv shows

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+
 import { connectDB } from "./config/db.js";
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 
 

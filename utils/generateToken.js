a4afc1c3c135c272f8417e26d0f2bd8d59abdae4
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-29 09:14:54 +0300

    fix token generation

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,9 +5,10 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("jwt-netflix", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
+		httpOnly: true, 
+		sameSite: "none", 
+		secure: process.env.NODE_ENV === "production",
+		secure: true,
 	});
 
 	return token;
